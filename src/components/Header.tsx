a0b3e245fcaaad6a191f1b456874b2645b51ec36
deekshithm0 <deekshithm0@gmail.com>
2025-01-13 00:46:46 +0530

    Favicons imported

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex flex-row w-full justify-between items-center xs:px-6 xs:text-xs md:px-4'>
+      <FontAwesomeIcon icon={faChevronLeft} />
+      <h1 className=''>PLAYING FROM PLAYLIST</h1>
+      <FontAwesomeIcon icon={faEllipsisVertical} />
+    </div>
   )
 }
 
--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShuffle } from '@fortawesome/free-solid-svg-icons'
+import { faBackwardStep } from '@fortawesome/free-solid-svg-icons'
+import { faPause } from '@fortawesome/free-solid-svg-icons'
+import { faForwardStep } from '@fortawesome/free-solid-svg-icons'
+import { faRepeat } from '@fortawesome/free-solid-svg-icons'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 const PlayerControls = () => {
   return (
-    <div>PlayerControls</div>
+    <div className='w-full flex flex-col items-center justify-center xs:gap-8 xs:pt-5 md:pt-10'>
+      <div className='w-full flex justify-evenly'>
+        <FontAwesomeIcon icon={faShuffle} />
+        <FontAwesomeIcon icon={faBackwardStep} />
+        <FontAwesomeIcon icon={faPause} />
+        <FontAwesomeIcon icon={faForwardStep} />
+        <FontAwesomeIcon icon={faRepeat} />
+      </div>
+      <div className='w-full flex justify-between xs:px-11'>
+        <FontAwesomeIcon icon={faDownload} />
+        <FontAwesomeIcon icon={faHeart} />
+      </div>
+    </div>
   )
 }
 
