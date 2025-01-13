46f79582bf01e8c4f6d1b2e22b2c4f8b54aed6b3
deekshithm0 <deekshithm0@gmail.com>
2025-01-13 12:04:40 +0530

    added json data

--- src/components/SongDetails.tsx
+++ src/components/SongDetails.tsx
@@ -4,7 +4,7 @@ const SongDetails = () => {
   return (
     <div className='h-full w-full xs:mx-6 md:m-2 rounded-3xl  bg-white bg-opacity-5 shadow-xl flex flex-col items-center'>
       <div className=" h-[80%]">
-        <img src="./soami.jpeg" alt="songImage" className="h-full md:h-[80%] xs:rounded-full" />
+        <img src="./images/soami.jpeg" alt="songImage" className="h-full md:h-[80%] xs:rounded-full" />
       </div>
       <h1 className='h-[10%] md:pt-6'>So am i</h1>
       <p className='h-[10%] text-xs'>Ava Max</p>
